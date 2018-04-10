3a3a569d2d0465b14bbe6c6d1f00a890f3e8f2a6
John Maina <jqhnmaina@outlook.com>
2018-04-10 18:47:39 +0300

    Implemented 5*5 and 3*3 layout

--- app/src/main/java/com/example/jqhn/climbchallenge/MainActivity.java
+++ app/src/main/java/com/example/jqhn/climbchallenge/MainActivity.java
@@ -1,7 +1,9 @@
 package com.example.jqhn.climbchallenge;
 
+import android.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
